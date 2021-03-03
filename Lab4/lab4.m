6695fd30a03b95b8bf24518b6b047225a09d8087
Matt Kreul <mjkreul@iastate.edu>
2021-03-02 19:27:54 -0600

    Prelim 2 of lab 4 code; adding lab 5 information as well

--- Lab4/lab4.m
+++ Lab4/lab4.m
@@ -7,8 +7,10 @@ P_atm = 99000; %pascals
 R = 287; %temp constant J/kg*K
 rho_water = 997; %density of water kg/m^3
 T = 22 + 273.15; %Kelvin
-rho = P_atm*R*T; %
-K = 1.1; % may need to change this
+rho = P_atm/(R*T); %density of air kg/m^3
+K = 1.14393; % may need to change this
+mu = 1.825*10^(-5); % dynamic viscosity of air
+dia = 0.7*0.0254; % diameter (2R) of the cyllinder
 
 %% Parsing the file values and averaging them
 % I moved all of the files into the working directory for easier access
@@ -27,22 +29,16 @@ P_A_P_EAvg = [];
 for i = 1:numFiles
     % import the table
     importedCSV = readtable(CSVFiles(i).name);
-    % grab the "first" 16 columns (there is a single column that is the ms)
-    first = importedCSV(:, 2:17); 
-    % grab the "second" 2 columns (there are a lot of values for
-    % temperature)
-    second = importedCSV(:, 35:36);
-    % concatenate them...
-    combinedTable = [first, second];
+    % grab the "first" 18 columns (there is a single column that is the ms)
+    pressureTable = importedCSV(:, 2:19); 
     % then change them from a table to an array... I fucking hate matlab...
-    combined = combinedTable{:,:};
+    pressure = pressureTable{:,:};
     % get the average of each
-    averagedP = mean(combined);
+    averagedP = mean(pressure);
     % then transpose it to make it a column
     pressureAvg(:,i) = averagedP';
-    
     % Get PA and PE and do the same thing to them
-    P_A_P_ETable = importedCSV(:,37:38);
+    P_A_P_ETable = importedCSV(:,20:21);
     P_A_P_E = P_A_P_ETable{:,:};
     averagedP_A_P_E = mean(P_A_P_E);
     P_A_P_EAvg(:,i) = averagedP_A_P_E';
@@ -50,7 +46,8 @@ end
 
 % Write the values to a csv file to import into LaTeX doc
 writetable(array2table(pressureAvg), 'outputFiles/avgP.csv', 'Delimiter', ';');
-writetable(array2table(P_A_P_EAvg), 'outputFiles/avgP_A_P_E.csv', 'Delimiter', ';');
+writetable(array2table(P_A_P_EAvg), 'outputFiles/avgP_A_P_E.csv',...
+    'Delimiter', ';');
 
 %% Get Dynamic Pressure and Velocity
 
@@ -60,27 +57,65 @@ q = [];
 v = [];
 for i = 1:numFiles
     P_AmP_E = P_A_P_EAvg(1, i) - P_A_P_EAvg(2, i);
-    
-    q(i) = P_AmP_E;
+    q(i) = K*P_AmP_E;
     v(i) = sqrt((2*P_AmP_E)/(rho * K));
-    
 end
 
 %% Get Cp
-
+% C_P = average pressure of each point/dynamic pressure at that air speed
+% C_P_theoretical = 1-4*sin(theta)*sin(theta)
 [rows, cols] = size(pressureAvg);
-Cp = zeros(rows, cols);
+C_P = zeros(rows, cols);
 for i = 1:cols
     for j = 1:rows
-        Cp(j, i) = pressureAvg(j,i)/q(i);
+        C_P(j, i) = pressureAvg(j,i)/q(i);
     end
 end
+C_P_theoretical = 1-4*sin((-180:5:180)*pi/180).*sin((-180:5:180)*pi/180);
+
+%% Get D 
+
+%integral -pi to pi, P*cos(theta)*R*dtheta
+% = -deltaTheta/2 * sum_{i = 1}^{n} C_pi*cos(theta)
+% start theta from 360 and subtract by 20 until theta = 0
+% 
     
+C_D = [];
+
+for i = 1:cols
+    theta = 360;
+    for j = 1:rows
+        sum = C_P(j,i)* cos(theta*(pi/180));
+        theta = theta - 20;
+    end
+    C_D(i) = sum*(-(20*pi/180)/2);
+end
     
-    
-    
-    
-    
+ 
+%% Get C_D as function of Re
+% Re = (rho*V_inf*D)/mu
+% D = C_D*rho*V_inf^2*2*R*0.5
+D = [];
+Re = [];
+for i = 1:cols
+    D(i) = C_D(i)*rho*(v(i)^2)*dia*0.5;
+    Re(i) = (rho*v(i)*D(i))/mu;
+end
+
+
+%% Plots
+
+for plotNum = 1:cols
+    figure(plotNum);
+    plot(C_P(:,plotNum));    
+end
+
+figure(plotNum);
+plot(C_D, Re)  
+xlabel("Coefficient of Drag");
+ylabel("Reynolds Number");
+title("Coefficient of Drag as a function of Reynolds Number");
+
 
 
 
